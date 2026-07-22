15ee2f417694c077469bc44aaf8c0c55ee27bed7
Nikolay Chernenko <nik.chernenko@vkteam.ru>
2026-07-22 10:42:38 +0300

    command tag handle

--- example/simple/main.go
+++ example/simple/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/max-messenger/max-bot-api-client-go/v2/model"
@@ -23,13 +24,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Handle("/info", func(c maxbot.Context) error {
+	bot.Handle("/help", func(c maxbot.Context) error {
 		kb := model.NewKeyboard()
 		kb.AddRow().
 			AddLink("docs", "https://dev.max.ru/docs").
 			AddCallBack("push me baby", "pushBtn")
 
 		err = c.Send("max мне в руки", maxbot.WithKeyboard(kb))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	bot.Handle("/command", func(c maxbot.Context) error {
+		command := c.Update().GetCommand()
+		msg := fmt.Sprintf(
+			"command: %s\nbot name: %s\n params: \n%s\n text: %s\n",
+			command.Command, command.BotName,
+			strings.Join(command.Params, "\n"),
+			command.RemainingText,
+		)
+		err = c.Send(msg)
 		if err != nil {
 			return err
 		}
@@ -73,10 +90,11 @@ func main() {
 	})
 
 	bot.Handle(maxbot.OnText, func(c maxbot.Context) error {
-		err = c.Send(fmt.Sprintf("%s - принято", c.Update().GetMessage().Body.Text))
-		if err != nil {
-			return err
-		}
+		//err = c.Send(fmt.Sprintf("%s - принято", c.Update().GetMessage().Body.Text))
+		//if err != nil {
+		//	return err
+		//}
+		fmt.Println("-->", c.Update().GetMessage().Body.Text)
 
 		return nil
 	})
